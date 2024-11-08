42192891986a422face5803a824e6c54aefb471d
bablu <bk147411@gmail.com>
2024-11-08 23:01:34 +0530

    Animation of wrrds

--- src/Components/Banner/BannerTwo.jsx
+++ src/Components/Banner/BannerTwo.jsx
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import selfImage from '../../Images/SelfImg6.png'
 import './BannerStyle.scss';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
-const BannerTwo = ({name}) => { 
+const BannerTwo = ({}) => { 
+const myName= "Aashutosh Raj"
+const position = "Front end Developer"
+   
+const gsapRef = useRef()
+
+useGSAP(()=>{
+    gsap.from(".firstName",{
+        y:50,
+        opacity:0,
+        stagger:0.3, 
+        duration:1,
+        delay:2.5,
+    })
+    gsap.from(".position",{
+        y:50,
+        opacity:0,
+        stagger:-0.3, 
+        duration:1,
+        delay:2.5,
+    })
+
+    gsap.from(".basedOn",{
+        y:50,
+        opacity:0,
+       
+        duration:1,
+        delay:5,
+    })
+})
 
 return (
     <> 
        <div className='heroContainer'>
             <div className='heroColumnLeftSide'>
-                <h1>
-                    <span className='splitName'>Aashutosh Raj</span>
-                </h1>
+            <h1>
+            {myName.split('').map((char, index) => (
+                <span key={char} className='firstName' ref={gsapRef}>{myName[index]}</span>
+            ))}
+            </h1>
                 <div className='basedOn'>
                         <p>Based on Punjab, INDIA</p>
                 </div>
@@ -22,7 +55,10 @@ return (
             </div>
             <div className='heroColumnRightSide'>
                 <h1>
-                    <span className='splitName'>Front end Developer</span>
+                        {position.split('').map((char, index) => (
+                        <span key={char} className='position'>{position[index]}</span>
+                    ))}
+                  
                 </h1>
             </div>
 
